1eac4d82eb7e3ceee099da90a23d08b40e635df7
RLBRKY <erol.berkay.cs@gmail.com>
2026-07-12 17:06:29 +0300

    Initial commit

---
+++ src/main/java/com/berkay/atm_simulation/AtmSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.berkay.atm_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkay/atm_simulation/AtmSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkay.atm_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
